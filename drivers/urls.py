1ef5f0c9eb30824ef0b9b80f2b2499e49b3ca6d1
justErkinov <uzcoding54@gmail.com>
2025-09-21 12:46:33 +0500

    Logout qo'shildi

--- drivers/urls.py
+++ drivers/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import IdentifyVehicleView, UserLoginView, UserRegistrationView
+from .views import IdentifyVehicleView, UserLoginView, UserRegistrationView, CustomLogoutView
 
 urlpatterns = [
     path("search/", IdentifyVehicleView.as_view(), name="search_car"),
     path("", UserLoginView.as_view(), name="login"),
+    path('logout/', CustomLogoutView.as_view(next_page='login'), name='logout'),
     path("register/", UserRegistrationView.as_view(), name='registration')
 ]
+
